20e3450e955b2cfda7883d8a9e4b17a4aff49db2
Tarik <tarikmerrylees@gmail.com>
2023-12-23 19:57:48 +0000

    componentise board and assessment

---
+++ src/lib/components/BriefAndAssessment.svelte
@@ -0,0 +1,83 @@
+<script>
+  export let image;
+  export let assessmentQuestions
+</script>
+
+<div class="brief-and-start-assessment">
+  <img class="board-brief" src="{image}" alt="Vatsim UK logo">
+    <div class="qa-container">
+      <h3>Pre-lesson Assessment</h3>
+      <!-- For each to loop through each assessment Question -->
+      {#each assessmentQuestions as question}
+      <div class="qa-item">
+        <div class="question">{question.question}</div>
+        <div class="answer">{question.answer}</div>
+      </div>
+      {/each}
+  </div>
+</div>
+
+<style>
+  .brief-and-start-assessment {
+    width: 100%;
+    display: flex;
+    gap: 2rem;
+  }
+
+.board-brief {
+  width: 50%;
+  display: block;
+  border: 2px solid #25ADE3;
+  border-radius: 2px;
+  object-fit: contain;
+  }
+
+  h3 {
+    margin-bottom: 1rem;
+    font-size: 2rem;
+    color:#17375E;
+  }
+
+  .qa-container {
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+  }
+
+  .qa-item {
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 10px;
+  }
+
+  .question, .answer {
+    padding: 10px;
+    border: 1px solid #17375E;
+    font-size: 1.2rem;
+  }
+
+  .question {
+    flex: 1;
+    background-color: #17375E;
+    color: #25ADE3;
+    border-right: none; /* Remove border between question and answer */
+  }
+
+  .answer {
+    flex: 2;
+    background-color: #fff;
+  }
+
+  @media screen and (max-width: 768px) {
+    .brief-and-start-assessment {
+      flex-direction: column;
+    }
+    .board-brief {
+      width: 100%;
+    }
+    .qa-container {
+      width: 100%;
+    }
+  }
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,21 @@
 <script>
+  import BriefAndAssessment from "$lib/components/BriefAndAssessment.svelte";
+
   import placeholder from '$lib/assets/placeholder.png'
+  const assessmentQuestions = [
+    {
+      question: "What is Flexbox?",
+      answer: "Flexbox is a CSS layout model optimized for designing complex layouts easily and predictably."
+    },
+    {
+      question: "Why use Flexbox?",
+      answer: "It allows for alignment of elements in different directions and orders, and is responsive and adaptable."
+    },
+  ]
 </script>
-<h2>Lesson Title</h2>
-<div class="brief-and-start-assessment">
-  <img class="board-brief" src="{placeholder}" alt="Vatsim UK logo">
-    <div class="qa-container">
-      <h3>Pre-lesson Assessment</h3>
-      <div class="qa-item">
-        <div class="question">What is Flexbox?</div>
-        <div class="answer">Flexbox is a CSS layout model optimized for designing complex layouts easily and predictably.</div>
-      </div>
-      <div class="qa-item">
-        <div class="question">Why use Flexbox?</div>
-        <div class="answer">It allows for alignment of elements in different directions and orders, and is responsive and adaptable.</div>
-      </div>
-      <!-- More Q&A items here -->
-  </div>
-</div>
+
+<h2>Lesson Deck Card</h2>
+<BriefAndAssessment image={placeholder} assessmentQuestions={assessmentQuestions}/>
 
 <style>
   h2 {
@@ -25,66 +24,6 @@
     font-weight: bold;
     color:#17375E;
   }
-  .brief-and-start-assessment {
-    width: 100%;
-    display: flex;
-    gap: 2rem;
-  }
-
-.board-brief {
-  width: 50%;
-  display: block;
-  border: 2px solid #25ADE3;
-  border-radius: 2px;
-  object-fit: contain;
-  }
-
-  h3 {
-    margin-bottom: 1rem;
-    font-size: 2rem;
-    color:#17375E;
-  }
-
-  .qa-container {
-    display: flex;
-    flex-direction: column;
-    width: 50%;
-  }
-
-  .qa-item {
-    display: flex;
-    flex-direction: row;
-    margin-bottom: 10px;
-  }
-
-  .question, .answer {
-    padding: 10px;
-    border: 1px solid #17375E;
-    font-size: 1.2rem;
-  }
-
-  .question {
-    flex: 1;
-    background-color: #17375E;
-    color: #25ADE3;
-    border-right: none; /* Remove border between question and answer */
-  }
-
-  .answer {
-    flex: 2;
-    background-color: #fff;
-  }
-
-  @media screen and (max-width: 768px) {
-    .brief-and-start-assessment {
-      flex-direction: column;
-    }
-    .board-brief {
-      width: 100%;
-    }
-    .qa-container {
-      width: 100%;
-    }
-  }
-
 </style>
+
+
