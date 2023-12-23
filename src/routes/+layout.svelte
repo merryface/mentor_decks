01d4fa0a5902f502c7504047442f12eddd0c5fcc
Tarik <tarikmerrylees@gmail.com>
2023-12-23 19:05:06 +0000

    header created

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,31 @@
-<script></script>
+<script>
+  import exampleImage from '$lib/assets/logo.png';
+</script>
 <header>
-  <h1>Layout</h1>
+  <img src="{exampleImage}" alt="Vatsim UK logo">
+  <h1>Instructor Deck</h1>
 </header>
 
 
 <slot />
+
+<style>
+  header {
+    background: #17375E;
+    color: #fff;
+    font-size: 2rem;
+    width: 100%;
+    height: 5rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+
+  img {
+    height: 3.5rem;
+    width: auto;
+    display: block;
+    }
+</style>
