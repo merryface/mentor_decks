b0f9d61a6f42a39b48cc02d796c3888e1e097685
Tarik <tarikmerrylees@gmail.com>
2023-12-23 19:23:14 +0000

    header created

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,30 @@
+<script>
+  import exampleImage from '$lib/assets/logo.png';
+</script>
+
+<header>
+  <img src="{exampleImage}" alt="Vatsim UK logo">
+  <h1>Instructor Deck</h1>
+</header>
+
+<style>
+  header {
+    background: #17375E;
+    color: #fff;
+    font-size: 2rem;
+    width: 100%;
+    height: 5rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+
+  img {
+    height: 3.5rem;
+    width: auto;
+    display: block;
+    }
+</style>
+
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,17 @@
 <script>
-  import exampleImage from '$lib/assets/logo.png';
+	import Header from "$lib/components/Header.svelte";
+
 </script>
-<header>
-  <img src="{exampleImage}" alt="Vatsim UK logo">
-  <h1>Instructor Deck</h1>
-</header>
 
+<Header />
 
-<slot />
+<div class="container">
+  <slot />
+</div>
 
 <style>
-  header {
-    background: #17375E;
-    color: #fff;
-    font-size: 2rem;
-    width: 100%;
-    height: 5rem;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding-left: 1rem;
-    padding-right: 1rem;
-  }
-
-  img {
-    height: 3.5rem;
-    width: auto;
-    display: block;
-    }
+.container {
+  padding: 2rem 3rem;
+  max-width: 1400px;
+}
 </style>
