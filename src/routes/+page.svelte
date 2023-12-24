22e15ef0f54dc22a52964fdfccb6ef17ad15683a
Tarik <tarikmerrylees@gmail.com>
2023-12-24 21:40:52 +0000

    create login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,47 +16,11 @@
   }
 </script>
 
-<form on:submit|preventDefault={handleLogin}>
-  <div>
-    <label for="username">Username:</label>
-    <input type="text" id="username" bind:value={username} />
-  </div>
-  <div>
-    <label for="password">Password:</label>
-    <input type="password" id="password" bind:value={password} />
-    <button type="submit">Login</button>
-  </div>
-  <p>This login page is temporarily just a mockup. Click login to access the website which is currently available to anyone.</p>
-</form>
+
+<button type="submit">Log in With VATSIM credentials</button>
+<p>This login page is temporarily just a mockup. Click login to access the website which is currently available to anyone.</p>
 
 <style>
-  form {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin: auto;
-    height: 100vh;
-    margin-top: 4rem;
-    width: 70%;
-  }
-
-  div {
-    display: flex;
-    flex-direction: column;
-  }
-
-  label {
-    margin-bottom: 0.5rem;
-    font-size: 1.2rem;
-  }
-
-  input {
-    padding: 0.5rem;
-    border: 1px solid #ccc;
-    border-radius: 0.25rem;
-    margin-bottom: 1rem;
-  }
-
   button {
     padding: 0.5rem;
     border: 1px solid #ccc;
@@ -64,6 +28,8 @@
     background-color: #fff;
     cursor: pointer;
     width: 100%;
+    height: 4rem;
+    margin-top: 4rem;
   }
 
   p {
