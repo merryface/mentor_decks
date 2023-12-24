59e25ea5bce61d38293d0795a60d7534a9db06a7
Tarik <tarikmerrylees@gmail.com>
2023-12-24 08:35:48 +0000

    create DeckCard component

---
+++ src/lib/components/DeckCard.svelte
@@ -0,0 +1,30 @@
+<script>
+  import BriefAndAssessment from "$lib/components/BriefAndAssessment.svelte";
+  import Exercise from "$lib/components/Exercise.svelte";
+  import FlightProfile from "$lib/components/FlightProfile.svelte";
+  import CriteriaSetup from "$lib/components/CriteriaSetup.svelte";
+
+  export let data;
+  const exercises = data.exercises;
+</script>
+
+<h2>Lesson Deck Card</h2>
+<BriefAndAssessment data={data}/>
+{#each exercises as exercise}
+<Exercise data={exercise} />
+{/each}
+
+<FlightProfile data={data.configuration} />
+
+<CriteriaSetup exercises={exercises} />
+  
+<style>
+  h2 {
+    margin-bottom: 1rem;
+    font-size: 3rem;
+    font-weight: bold;
+    color:#17375E;
+  }
+</style>
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,12 @@
 <script>
-  import BriefAndAssessment from "$lib/components/BriefAndAssessment.svelte";
-  import Exercise from "$lib/components/Exercise.svelte";
-  import FlightProfile from "$lib/components/FlightProfile.svelte";
   import { demoDeckCard } from '$lib/data/demoDeckCard.js'
-	import CriteriaSetup from "../lib/components/CriteriaSetup.svelte";
-
-  const exercises = demoDeckCard.exercises;
+  import DeckCard from '$lib/components/DeckCard.svelte';
 </script>
 
-<h2>Lesson Deck Card</h2>
-<BriefAndAssessment data={demoDeckCard}/>
-{#each exercises as exercise}
-<Exercise data={exercise} />
-{/each}
+<DeckCard data={demoDeckCard} />
 
-<FlightProfile data={demoDeckCard.configuration} />
-
-<CriteriaSetup exercises={exercises} />
-  
 <style>
-  h2 {
-    margin-bottom: 1rem;
-    font-size: 3rem;
-    font-weight: bold;
-    color:#17375E;
-  }
+
 </style>
 
 
