129666eaaf532e602e4eb89621fa127ca451d081
Tarik <tarikmerrylees@gmail.com>
2023-12-24 21:19:51 +0000

    create and style dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -4,3 +4,40 @@
   <a href="/mock">Mock Rating</a>
   <a href="/P3">P3 Rating</a>
 </nav>
+
+<style>
+nav {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-width: 90rem;
+  }
+
+  nav {
+    gap: 2rem;
+    margin-top: 4rem;
+  }
+
+  h2 {
+    margin-bottom: 1rem;
+    margin-top: 4rem;
+    font-size: 4rem;
+    font-weight: bold;
+    color:#17375E;
+    text-align: center;
+  }
+
+  a {
+    text-decoration: none;
+    background-color: #17375E;
+    font-size: 2rem;
+    color: #fff;
+    width: 100%;
+    max-width: 30rem;
+    padding: 1rem;
+    text-align: center;
+    border-radius: 5px;
+  }
+</style>
