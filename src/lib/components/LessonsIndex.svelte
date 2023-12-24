c1c898b0ba4ee6ce426eadf61becfbd0b597ffd0
Tarik <tarikmerrylees@gmail.com>
2023-12-24 21:55:32 +0000

    style lessonsIndex

--- src/lib/components/LessonsIndex.svelte
+++ src/lib/components/LessonsIndex.svelte
@@ -20,9 +20,42 @@
   });
 </script>
 
-<h1>{rating.charAt(0).toUpperCase() + rating.slice(1)} Rating</h1>
+<h2>{rating.charAt(0).toUpperCase() + rating.slice(1)} Rating</h2>
 <ul>
   {#each lessons as lesson}
     <li><a href={`/{rating}/${lesson.id}`}>{lesson.lessonTitle}</a></li>
   {/each}
 </ul>
+
+<style>
+  h2 {
+    margin-bottom: 2rem;
+    margin-top: 1rem;
+    font-size: 3rem;
+    font-weight: bold;
+    color:#17375E;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  li {
+    margin-bottom: 1rem;
+    font-size: 1.5rem;
+    font-weight: bold;
+    background-color:#17375E;
+    padding: 1rem;
+    width: 70%;
+    max-width: 60rem;
+    border-radius: 5px;
+    text-align: center;
+  }
+
+  a {
+    color: #fff;
+    text-decoration: none;
+  }
+</style>
